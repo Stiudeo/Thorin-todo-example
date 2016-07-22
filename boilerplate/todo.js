1ebadded34e6161e756332620928fa71c38ecd18
snupa <bunta.adrian@gmail.com>
2016-07-22 16:24:37 +0300

    initial commit

---
+++ boilerplate/todo.js
@@ -0,0 +1,7 @@
+'use strict';
+
+const thorin = require('thorin');
+
+thorin.run((err) => {
+  log.info("Running");
+});
---
+++ todo-app/app/actions/home.js
@@ -0,0 +1,8 @@
+'use strict';
+/**
+ * Created by Adrian on 22-Jul-16.
+ */
+const dispatcher = thorin.dispatcher;
+
+dispatcher
+  .addAction('home.landing');
